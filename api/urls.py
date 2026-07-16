4026ffa0f371e968c5786ad57656aceba99c3145
KalyaniChiliveri <125806586+KalyaniChiliveri@users.noreply.github.com>
2026-07-17 00:41:42 +0530

    Delete api directory

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SearchEventsView, UploadEventsView
-
-urlpatterns = [
-    path("upload/", UploadEventsView.as_view(), name="upload-events"),
-    path("search/", SearchEventsView.as_view(), name="search"),
-]
--- api/utils/ids.py
+++
@@ -1,4 +0,0 @@
-import uuid
-
-def generate_dataset_id() -> str:
-    return uuid.uuid4().hex
